b43b6e1422729eed6bc3265768f067e9ca04199d
Davide Di Modica <webmaster.ddm@gmail.com>
2015-11-02 15:17:14 +0100

    init demo

---
+++ js/app.js
@@ -0,0 +1,27 @@
+$(function(){		
+	//jqScroll
+    $('#stream').jqScroll({
+		heightOffset: 100, 
+		persistentLoad: false,
+		firstLoad: true,
+		loading: loadItems
+	});
+});
+
+function loadItems(){
+	if(!_JQLOADING){
+		_JQLOADING = 1;
+		$('#loading').show();	
+
+
+		var tpl = ''
+		for(var i=0; i<15; i++){
+			tpl+='<div>test</div>';
+		}
+		$('#jqScroll').append(tpl); 
+
+
+		$('#loading').hide();	
+		_JQLOADING = 0;
+	}
+}
