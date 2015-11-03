5649a29203f0a66694c4c17997c406150609738f
Davide Di Modica <webmaster.ddm@gmail.com>
2015-11-03 11:33:40 +0100

    style

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
 $(function(){		
 	//jqScroll
     $('#jqscroll').jqScroll({
-		heightOffset: 100, 
+		heightOffset: 200, 
 		persistentLoad: false,
 		firstLoad: true,
 		loading: loadItems
@@ -18,7 +18,7 @@ function loadItems(){
 		var tpl = ''
 		for(var i=0; i<15; i++){
 			var hex = "#000000".replace(/0/g,function(){return (~~(Math.random()*16)).toString(16);});
-			tpl+='<div style="background-color:'+hex+'">'+hex+'</div>';
+			tpl+='<div class="col-xs-6 col-sm-4 col-md-3 col-lg-2 item"><div style="background-color:'+hex+'"><span>'+hex+'</span><img src="data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw=="></div></div>';
 		}
 		$('#jqscroll').append(tpl); 
 
